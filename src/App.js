ffb891057a2967f349df6636f2d80bcb388e40fe
Mohammed <raouf.ahmed248@gmail.com>
2022-01-31 21:42:08 -0800

    added hls player to website

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import {NameForm} from "./NameForm.js"
 import {ProfilesList} from "./ProfilesList.js"
+import {VideoPlayer} from "./VideoPlayer.js"
+//import ReactHlsPlayer from 'react-hls-player';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header2">
         <p>ATOMIC HARMONICS APP</p>
+
+        <br></br>
+        <VideoPlayer/>
         <br></br>
       </header>
       <div className="inputForm-class">
---
+++ src/VideoPlayer.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Hls from "hls.js";
+export class VideoPlayer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.handleStartButton = this.handleStartButton.bind(this);
+        this.handleStopButton = this.handleStopButton.bind(this);
+        
+        
+      }
+      handleStartButton(event) {
+        const video = this.player;
+      const hls = new Hls();
+      const url = "http://localhost:4996/staticContent/static/videos/index.m3u8";
+ 
+      hls.loadSource(url);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED, function() {
+        console.log("playing now");  
+        video.play(); });
+    
+   
+    }
+    handleStopButton(event) {
+        const video = this.player;
+
+      video.pause(); 
+    
+   
+    }
+
+  componentDidMount() {
+
+  }
+  render() {
+    return (
+        <div className="App">
+          <input name="startVideoButton" type="button" value="StartVideo"  onClick={this.handleStartButton}/>
+          <input name="stopVideoButton" type="button" value="StopVideo"  onClick={this.handleStopButton}/>
+          
+          <video
+            className="videoCanvas"
+            ref={player => (this.player = player)}
+            autoPlay={true}
+            controls={true}
+          />
+        </div>
+    );
+  }
+}
